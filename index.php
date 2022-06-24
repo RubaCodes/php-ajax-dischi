264cc676ebb20c8f350f0726bae9361e5f5d41b0
rubacodes <rubacodes@gmail.com>
2022-06-24 13:38:17 +0200

    print corretto delle card

--- index.php
+++ index.php
@@ -18,8 +18,13 @@ require_once './server/database.php';
         <div class="container mx-auto">
             <section id="library">
                 <?php foreach ($database as $disk) { ?>
-                    <div class="card">
-                        <?php var_dump($disk) ?>
+                    <div class="card text-center p-5 bg-slate-700">
+                        <img class="w-full" src="<?php echo $disk['poster'] ?>" alt="<?php echo $disk['author'] ?>" />
+                        <h2 class="text-white uppercase text-2xl font-bold pt-5">
+                            <?php echo $disk['title'] ?>
+                        </h2>
+                        <div class="text-slate-400 capitalize text-xl pt-3"><?php echo $disk['author'] ?></div>
+                        <div class="text-slate-400 text-xl"><?php echo $disk['year'] ?></div>
                     </div>
                 <?php } ?>
             </section>
