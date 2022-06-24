ffbbe62d0b2094ac74d2775bb7520a7b7080f6cb
rubacodes <rubacodes@gmail.com>
2022-06-24 13:28:05 +0200

    layout iniziale main

--- index.php
+++ index.php
@@ -10,11 +10,20 @@ require_once './server/database.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php dischi</title>
 </head>
 
 <body>
+    <main>
+        <div class="container mx-auto">
+            <section id="library">
 
+
+            </section>
+
+        </div>
+
+    </main>
 </body>
 
 </html>
