68ddccdf413116cee52323b1d1cc66a7c4c572d6
rubacodes <rubacodes@gmail.com>
2022-06-24 16:34:42 +0200

    implementata call correttta api

--- server/api.php
+++ server/api.php
@@ -7,10 +7,38 @@ $genre = $_GET['genre'];
 
 if ($genre === 'All' || $genre === '') {
     echo json_encode($database);
-} elseif ($genres === 'Rock') {
+} elseif ($genre === 'Rock') {
+    $response = [];
+    foreach ($database as $album) {
+        if (array_search('Rock', $album)) {
+            $response[] = $album;
+        }
+    }
+    echo json_encode($response);
 } elseif ($genre === 'Pop') {
+    $response = [];
+    foreach ($database as $album) {
+        if (array_search('Pop', $album)) {
+            $response[] = $album;
+        }
+    }
+    echo json_encode($response);
 } elseif ($genre === 'Metal') {
+    $response = [];
+    foreach ($database as $album) {
+        if (array_search('Metal', $album)) {
+            $response[] = $album;
+        }
+    }
+    echo json_encode($response);
 } elseif ($genre === 'Jazz') {
+    $response = [];
+    foreach ($database as $album) {
+        if (array_search('Jazz', $album)) {
+            $response[] = $album;
+        }
+    }
+    echo json_encode($response);
 }
 
 
