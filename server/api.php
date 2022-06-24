329cfd687851d2012ed0efd0ddaa26fc9417a12b
rubacodes <rubacodes@gmail.com>
2022-06-24 17:38:24 +0200

    controllo se $_GET vuoto

--- server/api.php
+++ server/api.php
@@ -5,43 +5,43 @@ header('Content-Type: application/json');
 
 $genre = $_GET['genre'];
 
-if ($genre === 'All' || $genre === '') {
+if (!empty($_GET)) {
+    if ($genre === 'All' || $genre === '') {
+        echo json_encode($database);
+    } elseif ($genre === 'Rock') {
+        $response = [];
+        foreach ($database as $album) {
+            if (array_search('Rock', $album)) {
+                $response[] = $album;
+            }
+        }
+        echo json_encode($response);
+    } elseif ($genre === 'Pop') {
+        $response = [];
+        foreach ($database as $album) {
+            if (array_search('Pop', $album)) {
+                $response[] = $album;
+            }
+        }
+        echo json_encode($response);
+    } elseif ($genre === 'Metal') {
+        $response = [];
+        foreach ($database as $album) {
+            if (array_search('Metal', $album)) {
+                $response[] = $album;
+            }
+        }
+        echo json_encode($response);
+    } elseif ($genre === 'Jazz') {
+        $response = [];
+        foreach ($database as $album) {
+            if (array_search('Jazz', $album)) {
+                $response[] = $album;
+            }
+        }
+        echo json_encode($response);
+    }
+} else {
+    //se non e' stata fatta nessuan richiesta stampa tutto
     echo json_encode($database);
-} elseif ($genre === 'Rock') {
-    $response = [];
-    foreach ($database as $album) {
-        if (array_search('Rock', $album)) {
-            $response[] = $album;
-        }
-    }
-    echo json_encode($response);
-} elseif ($genre === 'Pop') {
-    $response = [];
-    foreach ($database as $album) {
-        if (array_search('Pop', $album)) {
-            $response[] = $album;
-        }
-    }
-    echo json_encode($response);
-} elseif ($genre === 'Metal') {
-    $response = [];
-    foreach ($database as $album) {
-        if (array_search('Metal', $album)) {
-            $response[] = $album;
-        }
-    }
-    echo json_encode($response);
-} elseif ($genre === 'Jazz') {
-    $response = [];
-    foreach ($database as $album) {
-        if (array_search('Jazz', $album)) {
-            $response[] = $album;
-        }
-    }
-    echo json_encode($response);
 }
-
-
-//print della risposta
-// $response = 
-// echo $response;
