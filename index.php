f23c144620cf4af2b42578a87a4f3095660db4bb
rubacodes <rubacodes@gmail.com>
2022-06-24 13:42:25 +0200

    push finale index.php

--- index.php
+++ index.php
@@ -10,13 +10,14 @@ require_once './server/database.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>php dischi</title>
 </head>
 
 <body>
-    <main>
+    <main class="bg-slate-800 h-[100vh] flex items-center">
         <div class="container mx-auto">
-            <section id="library">
+            <section class="grid grid-cols-5 gap-4" id="library">
                 <?php foreach ($database as $disk) { ?>
                     <div class="card text-center p-5 bg-slate-700">
                         <img class="w-full" src="<?php echo $disk['poster'] ?>" alt="<?php echo $disk['author'] ?>" />
